95cfa850ebd48d0582d0c168c941045938a941fa
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-12-05 02:14:10 +0100

    Add files via upload

---
+++ KonstruktoriStatickeVarijable/KonstruktoriStatickeVarijable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstruktoriStatickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa X = new PrvaKlasa("Paolo", "Beaković");
+            PrvaKlasa Y = new PrvaKlasa("Karlo", "Matošević");
+            PrvaKlasa Z = new PrvaKlasa();
+
+            Console.WriteLine(X + "\n" + Y + "\n" + Z);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KonstruktoriStatickeVarijable/KonstruktoriStatickeVarijable/PrvaKlasa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstruktoriStatickeVarijable
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+        private string ime, prezime;
+
+        public PrvaKlasa()
+        {
+            counter = 0;
+            counter++;
+        }
+       
+        public static int Brojač
+        { 
+            get => counter;
+        }
+        public string Ime
+        { 
+            get => Ime; set => Ime =value; 
+        } 
+        public string Prezime
+        {
+            get => Prezime; set => Prezime = value;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Redni br.: " + Ime + ", " + Prezime + " " + counter;
+
+            return ispis;
+        }
+
+
+    }
+
+}
